cf7694faa68b258b7d8aaf59b3f8c9237b3eef57
Irvin <162080822+IrvinEdgarSmith@users.noreply.github.com>
2025-06-19 05:14:50 -0500

    Add test tone toggle button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,12 +3,10 @@ import RootGrid from './components/layout/RootGrid';
 import SubtractiveSynth from './pages/synth/SubtractiveSynth';
 import CentralWorkspace from './pages/synth/CentralWorkspace';
 import AdditiveSynth from './pages/synth/AdditiveSynth';
-import TestTone from './components/TestTone';
 
 export default function App() {
   return (
     <>
-      <TestTone />
       <RootGrid>
         {/* Left Sidebar (Subtractive Synth) */}
         <SubtractiveSynth />
